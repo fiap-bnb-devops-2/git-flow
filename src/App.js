85dfd6e345a708638485bdcf9e0b4d1e9d7fc3af
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-09-03 10:24:41 -0300

    feat: login page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Este é o projeto que implementa o Git Flow.
         </p>
